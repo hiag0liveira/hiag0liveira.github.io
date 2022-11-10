9f54c3a8366c8c7c427ad722c29dae29a756172a
Hiago de Oliveira Mendes <99771272+hiagophd@users.noreply.github.com>
2022-11-10 11:51:20 -0300

    Update script.js

--- Projeto/index/script.js
+++ Projeto/index/script.js
@@ -11,27 +11,3 @@ function verificarCampoVazio(){
         }
     return true;
 }
-function checarEmail(){
-    if( document.forms[1].BuscarEmail.value=="" 
-    || document.forms[1].BuscarEmail.value.indexOf('@')==-1 
-    || document.forms[1].BuscarEmail.value.indexOf('.')==-1 )
-        {
-        alert( "Por favor, informe um E-MAIL válido!" );
-        return false;
-    }
-    return true;
-}
-function verificarCampoVazioEmail(){
-    if(document.getElementById("BuscarEmail").value.length < 3){
-        alert('Por favor, preencha o campo de E-MAIL a ser buscado');
-        return false;
-    }
-    if( document.forms[1].BuscarEmail.value=="" 
-    || document.forms[1].BuscarEmail.value.indexOf('@')==-1 
-    || document.forms[1].BuscarEmail.value.indexOf('.')==-1 )
-        {
-        alert( "Por favor, informe um E-MAIL válido!" );
-        return false;
-    }
-    return true;
-}
