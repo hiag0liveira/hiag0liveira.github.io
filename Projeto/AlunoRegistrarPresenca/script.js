2b28b72f9e977ba1bb90aea8c35e9dadae37aca5
Hiago Oliveira <99771272+hiag0liveira@users.noreply.github.com>
2023-09-24 17:01:40 -0300

    Delete Projeto directory

--- Projeto/AlunoRegistrarPresenca/script.js
+++
@@ -1,7 +0,0 @@
-function verificarCampoVazio(){
-    if(document.getElementById("BuscarCod").value.length < 1){
-        alert('Por favor, preencha o campo do codigo');
-        document.getElementById("BuscarCod").focus();
-        return false
-        }
-}
