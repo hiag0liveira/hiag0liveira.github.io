d50a784b92c38eee830345357549d181c9234f09
Hiago Oliveira <99771272+hiag0liveira@users.noreply.github.com>
2023-09-24 16:47:07 -0300

    Delete AtividadesHIHC directory

--- AtividadesHIHC/Ex2-Curso/capitulo2/exercicios/ex13/script.js
+++
@@ -1,5 +0,0 @@
-function somaP(){
-    var obterP = document.getElementById('pInicial1')
-    var lista_inputs = document.getElementsByTagName('input')
-    obterP.textContent+=lista_inputs[0].value
-}
